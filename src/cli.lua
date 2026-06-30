149220fc5af21426edd2ce9901cee67c26124f2f
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2026-06-29 23:41:41 -0400

    Autoformat everything

--- src/cli.lua
+++ src/cli.lua
@@ -1,110 +1,121 @@
 -- Give mods 5 frames for load-order shenanigans
 local done = false
 Balatest.internal.tq(function()
-    local function recurse(d)
-        Balatest.q(function()
-            if d == 0 then
-                done = true
-            else
-                recurse(d - 1)
-            end
-        end)
-    end
-    recurse(5)
+	local function recurse(d)
+		Balatest.q(function()
+			if d == 0 then
+				done = true
+			else
+				recurse(d - 1)
+			end
+		end)
+	end
+	recurse(5)
 end)
-Balatest.internal.tq(function() return done end)
+Balatest.internal.tq(function()
+	return done
+end)
 
 -- Arguments use colons because Lovely complains about equals
 local function parse_args()
-    local run = 0
-    local mod = nil
-    local category = nil
-    for _, v in ipairs(arg) do
-        if v == "--balatest-run-tests" then
-            if run ~= 0 and run ~= 1 then
-                error("Invalid Balatest CLI arguments")
-            end
-            run = 1
-        elseif v:sub(1, 21) == "--balatest-run-tests:" then
-            if run ~= 0 and run ~= 1 then
-                error("Invalid Balatest CLI arguments")
-            end
-            run = 1
-            mod = v:sub(22)
-        elseif v:sub(1, 20) == "--balatest-category:" then
-            if run ~= 0 and run ~= 1 then
-                error("Invalid Balatest CLI arguments")
-            end
-            run = 1
-            category = v:sub(21)
-        elseif v:sub(1, 20) == "--balatest-run-test:" then
-            if run ~= 0 and run ~= 2 then
-                error("Invalid Balatest CLI arguments")
-            end
-            run = 2
-            mod = v:sub(21)
-        end
-    end
+	local run = 0
+	local mod = nil
+	local category = nil
+	for _, v in ipairs(arg) do
+		if v == "--balatest-run-tests" then
+			if run ~= 0 and run ~= 1 then
+				error("Invalid Balatest CLI arguments")
+			end
+			run = 1
+		elseif v:sub(1, 21) == "--balatest-run-tests:" then
+			if run ~= 0 and run ~= 1 then
+				error("Invalid Balatest CLI arguments")
+			end
+			run = 1
+			mod = v:sub(22)
+		elseif v:sub(1, 20) == "--balatest-category:" then
+			if run ~= 0 and run ~= 1 then
+				error("Invalid Balatest CLI arguments")
+			end
+			run = 1
+			category = v:sub(21)
+		elseif v:sub(1, 20) == "--balatest-run-test:" then
+			if run ~= 0 and run ~= 2 then
+				error("Invalid Balatest CLI arguments")
+			end
+			run = 2
+			mod = v:sub(21)
+		end
+	end
 
-    return run, mod, category
+	return run, mod, category
 end
 
 -- Doesn't properly handle U+0020 or below, but who cares
 local function json_escape(str)
-    return (str or ""):gsub('\\', '\\\\'):gsub('"', '\\"')
+	return (str or ""):gsub("\\", "\\\\"):gsub('"', '\\"')
 end
 
 --- @param test string
 --- @param res Result
 local function json_log(test, res)
-    sendInfoMessage(
-        '{"test":"' .. json_escape(test) ..
-        '","skipped":' .. (res.skipped and 'true' or 'false') ..
-        ',"success":' .. (res.success and 'true' or 'false') ..
-        ',"reason":"' .. json_escape(res.reason) .. '"}',
-        "Balatest")
+	sendInfoMessage(
+		'{"test":"'
+			.. json_escape(test)
+			.. '","skipped":'
+			.. (res.skipped and "true" or "false")
+			.. ',"success":'
+			.. (res.success and "true" or "false")
+			.. ',"reason":"'
+			.. json_escape(res.reason)
+			.. '"}',
+		"Balatest"
+	)
 end
 
 -- Don't tq this so `error()`s will crash
-G.E_MANAGER:add_event(Event {
-    no_delete = true,
-    created_on_pause = true,
-    func = function()
-        local raw_love_errorhandler = love.errorhandler
-        function love.errorhandler(msg)
-            -- No return to exit immediately
-            raw_love_errorhandler(msg)
-        end
+G.E_MANAGER:add_event(
+	Event({
+		no_delete = true,
+		created_on_pause = true,
+		func = function()
+			local raw_love_errorhandler = love.errorhandler
+			function love.errorhandler(msg)
+				-- No return to exit immediately
+				raw_love_errorhandler(msg)
+			end
 
-        local run, mod, category = parse_args()
+			local run, mod, category = parse_args()
 
-        if run ~= 1 and run ~= 2 then
-            love.errorhandler = raw_love_errorhandler
-            return true
-        end
+			if run ~= 1 and run ~= 2 then
+				love.errorhandler = raw_love_errorhandler
+				return true
+			end
 
-        if run == 1 then
-            if category and not mod then
-                error("Invalid Balatest CLI arguments")
-            end
-            Balatest.run_tests(mod, category, json_log)
-            for k, v in pairs(Balatest.done) do
-                json_log(k, v)
-            end
-        elseif run == 2 then
-            Balatest.run_test(mod, json_log)
-        end
+			if run == 1 then
+				if category and not mod then
+					error("Invalid Balatest CLI arguments")
+				end
+				Balatest.run_tests(mod, category, json_log)
+				for k, v in pairs(Balatest.done) do
+					json_log(k, v)
+				end
+			elseif run == 2 then
+				Balatest.run_test(mod, json_log)
+			end
 
-        Balatest.internal.tq(function()
-            local fail = 0
-            for _, v in pairs(Balatest.done) do
-                if not v.skipped and not v.success then
-                    fail = fail + 1
-                end
-            end
-            love.event.quit(fail)
-        end)
+			Balatest.internal.tq(function()
+				local fail = 0
+				for _, v in pairs(Balatest.done) do
+					if not v.skipped and not v.success then
+						fail = fail + 1
+					end
+				end
+				love.event.quit(fail)
+			end)
 
-        return true
-    end
-}, 'Balatest')
+			return true
+		end,
+	}),
+	"Balatest"
+)
--- src/core.lua
+++ src/core.lua
@@ -2,10 +2,10 @@ Balatest = {}
 --- Used internally by Balatest to manage state. Don't touch this.
 Balatest.internal = {}
 
-assert(SMODS.load_file 'src/testplay.lua')()
-assert(SMODS.load_file 'src/events.lua')()
-assert(SMODS.load_file 'src/assertions.lua')()
-assert(SMODS.load_file 'src/cli.lua')()
+assert(SMODS.load_file("src/testplay.lua"))()
+assert(SMODS.load_file("src/events.lua"))()
+assert(SMODS.load_file("src/assertions.lua"))()
+assert(SMODS.load_file("src/cli.lua"))()
 
 --- @alias Result {success:true}|{success:false,reason:string?}|{skipped:true,reason:string}
 
@@ -46,157 +46,178 @@ Balatest.internal.kill = nil
 --- @param category? string The category to run tests from.
 --- @param after? fun(test_name: string, result: Result) The result handler for these tests. If unset, defaults to a simple logger.
 function Balatest.run_tests(mod, category, after)
-    local todo = {}
-    local allowed = nil
-    if category then
-        if not mod then
-            sendErrorMessage('No mod provided for categories. Aborting.', 'Balatest')
-            return
-        end
-        if not Balatest.tests_by_mod_and_category[mod] then
-            sendErrorMessage('That mod appears to not exist. Aborting. (Are you using your prefix instead of your ID?)',
-                'Balatest')
-            return
-        end
-        if not Balatest.tests_by_mod_and_category[mod][category] then
-            sendErrorMessage('That category appears to not exist in that mod. Aborting.', 'Balatest')
-            return
-        end
-        allowed = Balatest.tests_by_mod_and_category[mod][category]
-    elseif mod then
-        if not Balatest.tests_by_mod[mod] then
-            sendErrorMessage('That mod appears to not exist. Aborting. (Are you using your prefix instead of your ID?)',
-                'Balatest')
-            return
-        end
-        allowed = Balatest.tests_by_mod[mod]
-    end
+	local todo = {}
+	local allowed = nil
+	if category then
+		if not mod then
+			sendErrorMessage("No mod provided for categories. Aborting.", "Balatest")
+			return
+		end
+		if not Balatest.tests_by_mod_and_category[mod] then
+			sendErrorMessage(
+				"That mod appears to not exist. Aborting. (Are you using your prefix instead of your ID?)",
+				"Balatest"
+			)
+			return
+		end
+		if not Balatest.tests_by_mod_and_category[mod][category] then
+			sendErrorMessage("That category appears to not exist in that mod. Aborting.", "Balatest")
+			return
+		end
+		allowed = Balatest.tests_by_mod_and_category[mod][category]
+	elseif mod then
+		if not Balatest.tests_by_mod[mod] then
+			sendErrorMessage(
+				"That mod appears to not exist. Aborting. (Are you using your prefix instead of your ID?)",
+				"Balatest"
+			)
+			return
+		end
+		allowed = Balatest.tests_by_mod[mod]
+	end
 
-    if not allowed then
-        todo = Balatest.test_order
-    else
-        for _, v in ipairs(Balatest.test_order) do
-            if allowed[v] then todo[#todo + 1] = v end
-        end
-    end
+	if not allowed then
+		todo = Balatest.test_order
+	else
+		for _, v in ipairs(Balatest.test_order) do
+			if allowed[v] then
+				todo[#todo + 1] = v
+			end
+		end
+	end
 
-    local skip_count = 0
-    local real_todo = {}
-    for _, v in ipairs(todo) do
-        local should_skip = Balatest.should_skip(v)
-        if should_skip then
-            skip_count = skip_count + 1
-            Balatest.done[Balatest.tests[v].name] = { skipped = true, reason = should_skip }
-        else
-            real_todo[#real_todo + 1] = v
-        end
-    end
-    todo = real_todo
+	local skip_count = 0
+	local real_todo = {}
+	for _, v in ipairs(todo) do
+		local should_skip = Balatest.should_skip(v)
+		if should_skip then
+			skip_count = skip_count + 1
+			Balatest.done[Balatest.tests[v].name] = { skipped = true, reason = should_skip }
+		else
+			real_todo[#real_todo + 1] = v
+		end
+	end
+	todo = real_todo
 
-    Balatest.internal.done_count = 0
-    local prev_speed = G.SETTINGS.GAMESPEED
-    G.SETTINGS.GAMESPEED = 65536
-    sendInfoMessage(
-        'Running ' .. #todo .. ' tests...' .. (skip_count ~= 0 and (' (' .. skip_count .. ' skipped)') or ''),
-        'Balatest')
-    for _, v in ipairs(todo) do
-        Balatest.run_test(Balatest.tests[v], after)
-    end
-    Balatest.internal.tq(Event {
-        blocking = false,
-        no_delete = true,
-        func = function()
-            if Balatest.internal.done_count ~= #todo then return false end
+	Balatest.internal.done_count = 0
+	local prev_speed = G.SETTINGS.GAMESPEED
+	G.SETTINGS.GAMESPEED = 65536
+	sendInfoMessage(
+		"Running " .. #todo .. " tests..." .. (skip_count ~= 0 and (" (" .. skip_count .. " skipped)") or ""),
+		"Balatest"
+	)
+	for _, v in ipairs(todo) do
+		Balatest.run_test(Balatest.tests[v], after)
+	end
+	Balatest.internal.tq(Event({
+		blocking = false,
+		no_delete = true,
+		func = function()
+			if Balatest.internal.done_count ~= #todo then
+				return false
+			end
 
-            local pass, fail = 0, 0
-            skip_count = 0
-            for _, v in pairs(Balatest.done) do
-                if v.skipped then
-                    skip_count = skip_count + 1
-                elseif v.success then
-                    pass = pass + 1
-                else
-                    fail = fail + 1
-                end
-            end
-            sendInfoMessage(Balatest.internal.done_count .. ' tests ran.', 'Balatest')
-            sendInfoMessage(
-                pass ..
-                ' succeeded, ' .. fail .. ' failed' .. (skip_count == 0 and '.' or (', ' .. skip_count .. ' skipped.')),
-                'Balatest')
-            G.SETTINGS.GAMESPEED = prev_speed
-            return true
-        end
-    })
+			local pass, fail = 0, 0
+			skip_count = 0
+			for _, v in pairs(Balatest.done) do
+				if v.skipped then
+					skip_count = skip_count + 1
+				elseif v.success then
+					pass = pass + 1
+				else
+					fail = fail + 1
+				end
+			end
+			sendInfoMessage(Balatest.internal.done_count .. " tests ran.", "Balatest")
+			sendInfoMessage(
+				pass
+					.. " succeeded, "
+					.. fail
+					.. " failed"
+					.. (skip_count == 0 and "." or (", " .. skip_count .. " skipped.")),
+				"Balatest"
+			)
+			G.SETTINGS.GAMESPEED = prev_speed
+			return true
+		end,
+	}))
 end
 
---- The default logger for test results. 
+--- The default logger for test results.
 ---@param name string The test's name.
 ---@param res Result The rest's results.
 local function log_result(name, res)
-    if res.skipped then
-        sendWarnMessage('Test ' .. name .. ' skipped: ' .. res.reason, 'Balatest')
-    elseif res.success then
-        sendInfoMessage('Test ' .. name .. ' passed.', 'Balatest')
-    else
-        sendErrorMessage('Test ' .. name .. ' failed with: ' .. res.reason, 'Balatest')
-    end
+	if res.skipped then
+		sendWarnMessage("Test " .. name .. " skipped: " .. res.reason, "Balatest")
+	elseif res.success then
+		sendInfoMessage("Test " .. name .. " passed.", "Balatest")
+	else
+		sendErrorMessage("Test " .. name .. " failed with: " .. res.reason, "Balatest")
+	end
 end
 
 --- Runs a single test.
 --- @param test string|TestPlay The test to run.
 --- @param after? fun(test_name: string, result: Result) The result handler for this test. If unset, defaults to a simple logger.
 function Balatest.run_test(test, after)
-    if type(test) == 'string' then test = Balatest.tests[test] end
-    if test == nil then
-        sendWarnMessage('That test does not exist.', 'Balatest')
-        return
-    end
-    if not test.name then test.name = 'temporary' end
-    if not test.run_test then test = Balatest.TestPlay(test) end
+	if type(test) == "string" then
+		test = Balatest.tests[test]
+	end
+	if test == nil then
+		sendWarnMessage("That test does not exist.", "Balatest")
+		return
+	end
+	if not test.name then
+		test.name = "temporary"
+	end
+	if not test.run_test then
+		test = Balatest.TestPlay(test)
+	end
 
-    Balatest.internal.tq(function()
-        Balatest.done[test.name] = nil
-        Balatest.internal.abort = nil
-        Balatest.current_test = test.name
-        Balatest.current_test_object = test
+	Balatest.internal.tq(function()
+		Balatest.done[test.name] = nil
+		Balatest.internal.abort = nil
+		Balatest.current_test = test.name
+		Balatest.current_test_object = test
 
-        if Balatest.internal.kill then
-            Balatest.internal.abort = Balatest.internal.kill
-        end
-    end)
+		if Balatest.internal.kill then
+			Balatest.internal.abort = Balatest.internal.kill
+		end
+	end)
 
-    test:run_test()
+	test:run_test()
 
-    Balatest.internal.tq(function()
-        if Balatest.internal.kill and not Balatest.done[test.name] then
-            Balatest.done[test.name] = { skipped = true, reason = Balatest.internal.kill }
-            Balatest.internal.done_count = Balatest.internal.done_count + 1
-        elseif Balatest.internal.abort and not Balatest.done[test.name] then
-            Balatest.done[test.name] = { success = false, reason = type(Balatest.internal.abort) == 'string' and
-            Balatest.internal.abort or 'Aborted' }
-            Balatest.internal.done_count = Balatest.internal.done_count + 1
-        elseif not Balatest.done[test.name] then
-            Balatest.done[test.name] = { success = false, reason = 'Unknown' }
-            Balatest.internal.done_count = Balatest.internal.done_count + 1
-        end
-        (after or log_result)(test.name, Balatest.done[test.name])
-        Balatest.current_test = nil
-        Balatest.current_test_object = nil
-    end)
-    Balatest.internal.tq(function()
-        return Balatest.internal.hook_count == 0
-    end)
-    Balatest.internal.tq(function()
-        G.E_MANAGER.queues.Balatest_Run = {}
-    end)
+	Balatest.internal.tq(function()
+		if Balatest.internal.kill and not Balatest.done[test.name] then
+			Balatest.done[test.name] = { skipped = true, reason = Balatest.internal.kill }
+			Balatest.internal.done_count = Balatest.internal.done_count + 1
+		elseif Balatest.internal.abort and not Balatest.done[test.name] then
+			Balatest.done[test.name] = {
+				success = false,
+				reason = type(Balatest.internal.abort) == "string" and Balatest.internal.abort or "Aborted",
+			}
+			Balatest.internal.done_count = Balatest.internal.done_count + 1
+		elseif not Balatest.done[test.name] then
+			Balatest.done[test.name] = { success = false, reason = "Unknown" }
+			Balatest.internal.done_count = Balatest.internal.done_count + 1
+		end
+		(after or log_result)(test.name, Balatest.done[test.name])
+		Balatest.current_test = nil
+		Balatest.current_test_object = nil
+	end)
+	Balatest.internal.tq(function()
+		return Balatest.internal.hook_count == 0
+	end)
+	Balatest.internal.tq(function()
+		G.E_MANAGER.queues.Balatest_Run = {}
+	end)
 end
 
 --- Stops the current test batch.
 function Balatest.kill()
-    Balatest.internal.kill = "Tests were killed"
-    Balatest.internal.abort = Balatest.internal.kill
-    Balatest.internal.tq(function()
-        Balatest.internal.kill = nil
-    end)
+	Balatest.internal.kill = "Tests were killed"
+	Balatest.internal.abort = Balatest.internal.kill
+	Balatest.internal.tq(function()
+		Balatest.internal.kill = nil
+	end)
 end
